a03b9d6c8b69e83d9f33ddda369c7794ed367a15
“senarep“ <rbenavidesb@misena.edu.co>
2026-02-24 11:02:52 -0500

    Progrma menu con switch case

---
+++ script.js
@@ -0,0 +1,32 @@
+function mostrarDia() {
+    var numero = parseInt(document.getElementById("numeroDia").value);
+    var dia;
+
+    switch (numero) {
+        case 1:
+            dia = "Lunes";
+            break;
+        case 2:
+            dia = "Martes";
+            break;
+        case 3:
+            dia = "Miércoles";
+            break;
+        case 4:
+            dia = "Jueves";
+            break;
+        case 5:
+            dia = "Viernes";
+            break;
+        case 6:
+            dia = "Sábado";
+            break;
+        case 7:
+            dia = "Domingo";
+            break;
+        default:
+            dia = "Número inválido. Debe ser entre 1 y 7.";
+    }
+
+    document.getElementById("resultado").innerHTML = dia;
+}
